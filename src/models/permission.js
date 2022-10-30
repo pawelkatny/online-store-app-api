e3f1ba97958ccbdf4e58615a66d1f3c2b1facac5
Paweł Kątny <pawel.katny@gmail.com>
2022-10-30 13:09:00 +0100

    Add permission schema

--- src/models/permission.js
+++ src/models/permission.js
@@ -1,1 +1,23 @@
 const mongoose = require('mongoose');
+
+const permissionSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please provide name for new permission.'],
+        maxlength: 50
+    },
+
+    description: {
+        type: String,
+        required: [true, 'Please provide description for new permission.'],
+        maxlength: 100
+    },
+
+    tag: {
+        type: String,
+        required: [true, 'Permission tag field is required.'],
+        maxlength: 50
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Permission', permissionSchema);
