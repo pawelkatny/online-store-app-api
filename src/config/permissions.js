0a4178343869f1ab85ff386340067d058efcec6c
Paweł Kątny <pawel.katny@gmail.com>
2022-10-30 16:12:51 +0100

    Add basic permissions list for loading on server startup

---
+++ src/config/permissions.js
@@ -0,0 +1,62 @@
+const defaultPermissions = [
+    {
+        name: 'Can view any order(s).',
+        tag: 'view-order'
+    },
+    {
+        name: 'Can view any payment(s).',
+        tag: 'view-payment'
+    },
+    {
+        name: 'Can view any cart(s).',
+        tag: 'view-cart'
+    },
+    {
+        name: 'Can view any user(s).',
+        tag: 'view-user'
+    },
+    {
+        name: 'Can view any role(s).',
+        tag: 'view-role'
+    },
+    {
+        name: 'Can view any permission(s).',
+        tag: 'view-permission'
+    },
+    {
+        name: 'Can view any return(s).',
+        tag: 'view-return'
+    },
+    {
+        name: 'Can view any conversation(s).',
+        tag: 'view-conversation'
+    },
+    {
+        name: 'Can edit any order(s).',
+        tag: 'edit-order'
+    },
+    {
+        name: 'Can edit any cart(s).',
+        tag: 'edit-cart'
+    },
+    {
+        name: 'Can edit any user(s).',
+        tag: 'edit-role'
+    },
+    {
+        name: 'Can edit any permission(s).',
+        tag: 'edit-permission'
+    },
+    {
+        name: 'Can edit any product(s).',
+        tag: 'edit-product'
+    },
+    {
+        name: 'Can edit any conversation(s).',
+        tag: 'edit-conversation'
+    },
+    {
+        name: 'Can edit any file(s).',
+        tag: 'edit-file'
+    },
+]
