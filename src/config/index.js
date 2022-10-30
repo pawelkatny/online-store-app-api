8843b1130cc7c9577bd811c447dbdd1ccd32e1e9
Paweł Kątny <pawel.katny@gmail.com>
2022-10-30 19:30:06 +0100

    Add "morgan" package and configuration
    Writes access logs to specified log file

--- src/config/index.js
+++ src/config/index.js
@@ -1,11 +1,21 @@
 require('dotenv').config({ path: `${process.cwd()}/src/.env`});
+const path = require('path');
+const workingDir = process.cwd();
 
 const config = {
     api: {
         prefix: '/api/v1'
     },
+    absPath: workingDir,
     env: process.env.NODE_ENV || 'development',
     dbUri: process.env.MONGO_URI,
+    logs: {
+        access: {
+            file: 'access.log',
+            interval: '1d',
+            dir: path.join(workingDir, 'logs')
+        }
+    },
     mongoOpts: {
         useNewUrlParser: true
     },
