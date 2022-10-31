2fdec91ebd9ae76505c484e9a4a1b60844c245fc
Paweł Kątny <pawel.katny@gmail.com>
2022-10-31 23:10:54 +0100

    Update auth service
    * add user login
    * add login controller

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -2,11 +2,16 @@ const AuthService = require('../services/auth');
 const { StatusCodes } = require('http-status-codes');
 
 const register = async (req, res) => {
-    const { user, token } = await AuthService.register(userData);
+    const { email, name, password } = req.body;
+    const { user, token } = await AuthService.register({ email, name, password, roleName: 'customer' });
 
     res.status(StatusCodes.CREATED).json({ user, token });
 }
 
+const login = async (req, res) => {
+    const { email, password } = req.body;
+    const { user, token } = AuthService.login({ email, password });
+}
 module.exports = {
     register
 }
