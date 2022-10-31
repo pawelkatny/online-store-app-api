45c4eb7be14e9f95e5d185153c93ed388647a981
Paweł Kątny <pawel.katny@gmail.com>
2022-10-31 18:22:24 +0100

    Update user model
    * add schema "pre save" function to hash user password
    * add schema function to compare provided password with user`s hashed password
    * add schema function to create JWT

---
+++ src/services/auth.js
@@ -0,0 +1,16 @@
+const { User } = require('../models/user');
+
+class AuthService {
+    constructor() {
+        this.User = User;
+    }
+
+    async register(userData) {
+        
+    }
+
+    async login(loginData) {
+
+    }
+
+}
