18fbdc92b3a1f5b297617b0ec5f83793fab31d85
Paweł Kątny <pawel.katny@gmail.com>
2022-11-03 21:38:18 +0100

    Add product schema and model

---
+++ src/models/product.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Product name is required.'],
+        maxlength: 50
+    },
+    description: {
+        type: String,
+        required: [true, 'Product description is required.'],
+        maxlength: 10000
+    },
+    additionalInfo: [{
+        type: String,
+        maxlength: 500
+    }],
+    tags: [String],
+    images: [String],
+    quantity: Number
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
