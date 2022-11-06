252297f38e34afa7b1533da966f8dec47e8cafb0
Paweł Kątny <pawel.katny@gmail.com>
2022-11-06 20:13:07 +0100

    Add customer service
    Moved part of functions related only to customer role from user service

--- src/services/user.js
+++ src/services/user.js
@@ -1,6 +1,6 @@
 const { default: mongoose } = require('mongoose');
 const role = require('../models/role');
-const { User, Customer } = require('../models/user');
+const { User } = require('../models/user');
 
 class UserService {
 
@@ -65,53 +65,6 @@ class UserService {
         const password = new mongoose.Types.ObjectId();
         const { name, email, roleName } = userData;
         return User.create({ name, email, password, roleName });
-    }
-
-    static async getCustomerInfo(userId) {
-        return Customer.findById(userId);
-    }
-
-    static async getCustomerCart(userId) {
-        return Customer.findById(userId, { cart: 1 });
-    }
-
-    static async updateCustomerCart(userId, cartData) {
-        return Customer.findByIdAndUpdate(userId, { $set: cartData });
-    }
-
-    static async getCustomerAddress(userId, addressId) {
-        return Customer.findOne({
-            _id: userId,
-            addresses: {
-                _id: addressId
-            }
-        });
-    }
-
-    static async getCustomerAddresses(userId) {
-        return Customer.findById(userId, {
-            addresses: 1 
-        });
-    }
-
-    static async addCustomerAddress(userId, addressData) {
-        const addressId = new mongoose.Types.ObjectId();
-        return Customer.findByIdAndUpdate(userId, 
-            { $push: { addresses: { ...addressData, _id: addressId }}});
-    }
-
-    static async updateCustomerAddress(userId, addressId, addressData) {
-        return Customer.updateOne({ _id: userId, addresses: { _id: addressId } }, { $set: addressData });
-    }
-
-    static async deleteCustomerAddress(userId, addressId) {
-        return Customer.findByIdAndUpdate(userId, {
-            $pull: {
-                addresses: {
-                    _id: addressId
-                }
-            }
-        });
     }
 }
 
