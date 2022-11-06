088adbc4d3c7b6a0467780a13c51cd4e0b6f0278
Paweł Kątny <pawel.katny@gmail.com>
2022-11-06 13:56:12 +0100

    Update user service
    * add functions to alter car and address of users with 'customer' role

--- src/services/user.js
+++ src/services/user.js
@@ -1,19 +1,19 @@
 const role = require('../models/role');
-const { User } = require('../models/user');
+const { User, Customer } = require('../models/user');
 
 class UserService {
-    
+
     static async mapUserToObj(userId) {
         const user = await User.findById(userId)
-        .populate({ 
-            path: 'role', 
-            model: 'Role',
-            populate: {
-                path: 'permissions',
-                model: 'Permission',
-                select: 'tag'
-            }
-        });
+            .populate({
+                path: 'role',
+                model: 'Role',
+                populate: {
+                    path: 'permissions',
+                    model: 'Permission',
+                    select: 'tag'
+                }
+            });
 
         if (!user) {
             throw Error('User not found.');
@@ -53,6 +53,45 @@ class UserService {
 
     static async createUser(userData) {
         return User.create({ userData });
+    }
+
+    static async getCustomerInfo(userId) {
+        return Customer.findById(userId);
+    }
+
+    static async getCustomerCart(userId) {
+        return Customer.findById(userId, { cart: 1 });
+    }
+
+    static async updateCustomerCart(userId, cartData) {
+        return Customer.findByIdAndUpdate(userId, { $set: cartData });
+    }
+
+    static async getCustomerAddress(userId, addressId) {
+        return Customer.findOne({
+            _id: userId,
+            addresses: {
+                _id: addressId
+            }
+        });
+    }
+
+    static async addCustomerAddress(userId, addressData) {
+        return Customer.findById(userId).addAddress(addressData);
+    }
+
+    static async updateCustomerAddress(userId, addressId, addressData) {
+        return Customer.updateOne({ _id: userId, addresses: { _id: addressId } }, { $set: addressData });
+    }
+
+    static async deleteCustomerAddress(userId, addressId) {
+        return Customer.findByIdAndUpdate(userId, {
+            $pull: {
+                addresses: {
+                    _id: addressId
+                }
+            }
+        });
     }
 }
 
