168c767c2515213b7d098b9dbb7651c7f5ce0973
Paweł Kątny <pawel.katny@gmail.com>
2022-11-08 21:22:45 +0100

    Add product routes
    * update product model - name as unique field
    * update product controller

--- src/api/v1/routes/product.js
+++ src/api/v1/routes/product.js
@@ -1,8 +1,15 @@
 const { Router } = require('express');
 const router = Router();
+const productCtr = require('../../../controllers/product');
+const { isAuthenticated } = require('../../../middleware/auth');
 
-router.route('/').get((req, res) => {
-    res.status(200).json({ msg: 'Get Products '});
-});
+router.route('/')
+    .get(productCtr.getProducts)
+    .post(isAuthenticated, productCtr.createProduct);
+
+router.route('/:productId')
+    .get(productCtr.getProduct)
+    .patch(isAuthenticated, productCtr.updateProduct)
+    .delete(isAuthenticated, productCtr.deleteProduct);
 
 module.exports = router;
