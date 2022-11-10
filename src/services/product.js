bc51346f3f611d69442eec8c841f6d3a681b7328
Paweł Kątny <pawel.katny@gmail.com>
2022-11-10 22:07:02 +0100

    Update product service
    * add function to create new product review

--- src/services/product.js
+++ src/services/product.js
@@ -1,15 +1,18 @@
 const Product = require('../models/product');
+const Review = require('../models/review');
+const { Customer } = require('../models/user');
+const CustomError = require('../error/customError');
 
 class ProductService {
     static async getProducts(params) {
         return Product.find(params);
     }
 
-    static getProduct(productId) {
+    static async getProduct(productId) {
         return Product.findById(productId);
     }
 
-    static createProduct(productData) {
+    static async createProduct(productData) {
         const { name, description, additionalInfo, tags, quantity, price } = productData;
         return Product.create({
             name,
@@ -21,7 +24,7 @@ class ProductService {
         });
     }
 
-    static updateProduct(productId, productData) {
+    static async updateProduct(productId, productData) {
         const { name, description, additionalInfo, tags, quantity, price } = productData;
         return Product.findByIdAndUpdate(productId, {
             name,
@@ -33,10 +36,26 @@ class ProductService {
         });
     }
 
-    static deleteProduct(productId) {
+    static async deleteProduct(productId) {
         return Product.findByIdAndDelete(productId);
     }
 
+    static async addReview(productId, customerId, reviewData) {
+        const { rating, summary } = reviewData;
+        const customer = await Customer.findById(customerId); 
+
+        const review = {
+            reviewer: {
+                name: customer.name.first,
+                customer: customerId
+            },
+            product: productId,
+            summary: summary,
+            rating: rating
+        };
+
+        return Review.create(review);
+    } 
 }
 
 module.exports = ProductService;
