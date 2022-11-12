06f3447420fe628f126b8c8315994ab1629f3e7e
Paweł Kątny <pawel.katny@gmail.com>
2022-11-12 17:36:34 +0100

    Add orders route

--- src/api/v1/routes/order.js
+++ src/api/v1/routes/order.js
@@ -1,8 +1,13 @@
 const { Router } = require('express');
 const router = Router();
+const orderCtr = require('../../../controllers/order');
+const { isAuthenticated, isCustomer } = require('../../../middleware/auth');
 
-router.route('/').get((req, res) => {
-    res.status(200).json({ msg: 'Get Orders '});
-});
+router.route('/')
+    .get(isAuthenticated, orderCtr.getOrders)
+    .post(isCustomer, orderCtr.createOrder);
+
+router.route('/:orderId')
+    .get(isAuthenticated, orderCtr.getOrder);
 
 module.exports = router;
