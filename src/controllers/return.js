f31f5d42ed113cf810af8992a9413018207c505c
Paweł Kątny <pawel.katny@gmail.com>
2022-11-23 21:21:00 +0100

    Update return controller
    * add create return function

--- src/controllers/return.js
+++ src/controllers/return.js
@@ -28,3 +28,36 @@ const getReturn = async (req, res) => {
 
     res.status(StatusCodes.OK).json({ returnDoc }); 
 }
+
+const createReturn = async (req, res) => {
+    const currentUser = req.user;
+    const { orderId, products, reason } = req.body;
+    let customerId;
+
+    if (!currentUser.hasRole('customer') && !currentUser.hasPermission('edit-return')) {
+        throw new CustomError('Unauthorized', StatusCodes.UNAUTHORIZED);
+    } 
+
+    if (currentUser.hasRole('customer')) {
+        customerId = currentUser.id;
+    } 
+
+    if (currentUser.hasPermission('edit-return')) {
+        let { custId } = req.body;
+        customerId = custId;
+    } 
+
+    const newReturn = await ReturnService.createReturn(customerId, { orderId, products, reason });
+
+    if (!newReturn) {
+        throw new CustomError('Something went wrong', StatusCodes.INTERNAL_SERVER_ERROR);
+    }
+
+    res.status(StatusCodes.CREATED).json({ newReturn });
+}
+
+module.exports = {
+    getReturn,
+    getReturns,
+    createReturn
+}
