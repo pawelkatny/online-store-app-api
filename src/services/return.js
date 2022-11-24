4872fa6dae15d91fcef6e3c97ae0401d5786aba2
Paweł Kątny <pawel.katny@gmail.com>
2022-11-24 21:35:20 +0100

    Update return routes and service
    * fix wrong route module name for /return
    * update return create function

--- src/services/return.js
+++ src/services/return.js
@@ -6,36 +6,57 @@ class ReturnService {
         const { orderId, products, reason } = returnData;
 
         //get order data 
-        const order = await Order.findById(orderId, { number: 1 });
+        const order = await Order.findById(orderId, { number: 1 , products: 1 });
         //get returns connected to order and map to products
         const returns = await Return.find({ order: orderId }).populate('order').sort({ createdAt: 'desc' });
-
         //check if items were already returned
-        let returnedProducts;
-        returns.forEach(r => {
-            products.forEach(p => {
-                const productReturn = r.products.find(pr => pr.product == p.productId);
-                const productOrder = r.order.products.find(po => po.products.product == p.productId);
-                //check if product exists on order
-                const returnedProductIndex = returnedProducts.findIndex(rp => rp.productId == p.productId);
-                if (!returnedProductIndex) {
-                    returnedProducts.push(
-                        {
-                            ...p,
-                            returnedQty: productReturn.quantity,
-                            orderQty: productOrder.quantity,
-                            name: productOrder.name,
-                            price: productOrder.price
-                        }
-                    );
-                } else {
-                    returnedProducts[returnedProductIndex].returnedQty += productReturn.quantity; 
-                }
-                 
-            });
-        });
+        let returnedProducts = [];
 
-        const returnAvailability = returnedProducts.map(rp => {
+        if (returns.length == 0) {
+            products.forEach(p => {
+                const productOrder = order.products.find(po => po.product == p.productId);
+                if (productOrder) {
+                    returnedProducts.push({
+                        ...p, 
+                        returnedQty: 0,
+                        orderQty: productOrder.quantity,
+                        name: productOrder.name,
+                        price: productOrder.price 
+                    });
+                }
+            });
+        }
+
+        if (returns.length > 0) {
+            returns.forEach(r => {
+                products.forEach(p => {
+                    const productReturn = r.products.find(pr => pr.product == p.productId);
+                    const productOrder = order.products.find(po => po.product == p.productId);
+                    //check if product exists on order
+                    const returnedProductIndex = returnedProducts.findIndex(rp => rp.productId == p.productId);
+                    console.log(returnedProductIndex)
+                    if (returnedProductIndex < 0) {
+                        console.log('test')
+                        returnedProducts.push(
+                            {
+                                ...p,
+                                returnNumber: r.number,
+                                returnId: r._id,
+                                returnedQty: productReturn.quantity,
+                                orderQty: productOrder.quantity,
+                                name: productOrder.name,
+                                price: productOrder.price
+                            }
+                        );
+                    } else {
+                        returnedProducts[returnedProductIndex].returnedQty += productReturn.quantity; 
+                    }
+                     
+                });
+            });
+        }
+
+        const productsToReturn = returnedProducts.map(rp => {
             const qtyToReturn = rp.qty;
             const availableQtyToReturn = rp.orderQty - rp.returnedQty;
             let canBeReturned = false;
@@ -47,29 +68,33 @@ class ReturnService {
                 ...rp,
                 canBeReturned
             }
-        }).filter(rp => rp.canBeReturned == true);
+        });
+
+        const eligibleForReturn = productsToReturn.filter(rp => rp.canBeReturned == true);
+
+        const ineligibleForReturn = productsToReturn.filter(rp => rp.canBeReturned != true);
 
         //create default object to return from function
         const returnCreationStatus = {
             success: false,
-            returnData: null,
+            return: null,
         }
 
         //create return number
         let returnNumber = `R/${order.number}/1`;
         if (returns.length > 0) {
             const lastReturnId = returns[0].number.split('/')[4];
-            returnNumber = `R/${order.number}/${lastReturnId + 1}`;
+            returnNumber = `R/${order.number}/${+lastReturnId + 1}`;
         }
 
-        if (returnAvailability.length > 0) {
-            const newReturn = {
+        if (eligibleForReturn.length > 0) {
+            const returnData = {
                 number: returnNumber,
                 customer: customerId,
                 order: orderId,
                 reason: reason,
                 status: 'Processing',
-                products: returnAvailability.map(ra => {
+                products: eligibleForReturn.map(ra => {
                     return {
                         name: ra.name,
                         product: ra.productId,
@@ -81,10 +106,15 @@ class ReturnService {
                 })
             }
 
-            returnCreationStatus.status = true;
-            returnCreationStatus.returnData = newReturn;
+            const newReturn = await Return.create(returnData);
+            returnCreationStatus.success = true;
+            returnCreationStatus.return = newReturn;
         }
         
+        if (ineligibleForReturn.length > 0) {
+            returnCreationStatus.ineligibleForReturn = ineligibleForReturn;
+        }
+
         return returnCreationStatus;
     }
 
