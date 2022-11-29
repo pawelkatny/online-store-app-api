de06294a8a5e1c7efff3c16b5b098d4830cf25cb
Paweł Kątny <pawel.katny@gmail.com>
2022-11-29 22:15:43 +0100

    Update admin controller
    * add function to check mailer config

--- src/controllers/admin.js
+++ src/controllers/admin.js
@@ -1,9 +1,33 @@
 const { StatusCodes } = require('http-status-codes');
-
+const MailerService = require('../services/mailer');
 const dashboard = async (req, res) => {
     res.status(200).json({ user: req.user });
 }
 
+const checkMailerConfig = async (req, res) => {
+    const currentUser = req.user;
+    const mailer = new MailerService();
+    const emailOptions = {
+        header: {
+            to: currentUser.email,
+            subject: 'Testing Mailer Configuration',
+            template: 'test',
+            context: {
+                name: currentUser.name.first
+            }
+        }
+    }
+
+    const email = await mailer.sendEmail(emailOptions);
+
+    if (!email) {
+        throw new CustomError('Something went wrong', StatusCodes.INTERNAL_SERVER_ERROR);
+    }
+
+    res.status(StatusCodes.OK).send();
+}
+
 module.exports = {
-    dashboard
+    dashboard,
+    checkMailerConfig
 }
