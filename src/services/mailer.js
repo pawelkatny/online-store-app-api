f92d682f3ad1de9552b1fc33fd01a66026772eba
Paweł Kątny <pawel.katny@gmail.com>
2022-11-29 22:02:11 +0100

    Update mailer service
    * add handlebard as email template engine
    * add send email function

--- src/services/mailer.js
+++ src/services/mailer.js
@@ -1,6 +1,9 @@
 const { installed: { client_id, client_secret } } = require('../config/mailer/credentials.json');
-const { refresh_token, access_token, expiry_date } = require('../config/mailer/token.json');
+const { refresh_token, access_token, expiry_date } = require('../config/mailer/token.json');\
+const path = require('path');
 const nodemailer = require('nodemailer');
+const nmHandlebars = require('nodemailer-express-handlebars');
+
 
 class MailerService {
     constructor() {
@@ -8,6 +11,13 @@ class MailerService {
     }
 
     createTransporter() {
+        nhbs = nmHandlebars({
+            viewEngine: {
+                defaultLayout: false
+            },
+            viewPath: path.resolve(__dirname, '../templates/email')
+        });
+
         const transporter = nodemailer.createTransport({
             service: 'gmail',
             auth: {
@@ -23,14 +33,18 @@ class MailerService {
             from: 'Test account <pawel.katny.dev@gmail.com>'
         });
 
+        transporter.use('compile', nhbs);
         this.transporter = transporter;
     }
 
-    loadEmailTemplate() {
+    async sendEmail(options) {
+        const message = {
+            to: options.header.to,
+            subject: options.header.subject,
+            template: options.template,
+            context: { ...options.context }
+        }
 
-    }
-
-    sendEmail(options) {
-        
+        return this.transporter.sendMail(message);
     }
 }
