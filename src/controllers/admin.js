35c624b201e390a477c8d1d0a7bb7660cd44a5f2
Paweł Kątny <pawel.katny@gmail.com>
2022-12-05 22:34:44 +0100

    Add test email template for testing mailer service

--- src/controllers/admin.js
+++ src/controllers/admin.js
@@ -1,20 +1,27 @@
 const { StatusCodes } = require('http-status-codes');
 const MailerService = require('../services/mailer');
+const CustomError = require('../error/customError');
+
 const dashboard = async (req, res) => {
     res.status(200).json({ user: req.user });
 }
 
 const checkMailerConfig = async (req, res) => {
     const currentUser = req.user;
+    currentUser.email = 'pawel.katny@gmail.com'
+    if (!currentUser.hasRole('admin')) {
+        throw new CustomError('Unathorized', StatusCodes.UNAUTHORIZED);
+    }
+
     const mailer = new MailerService();
     const emailOptions = {
         header: {
             to: currentUser.email,
             subject: 'Testing Mailer Configuration',
-            template: 'test',
-            context: {
-                name: currentUser.name.first
-            }
+        },
+        template: 'test',
+        context: {
+            name: currentUser.name.first
         }
     }
 
