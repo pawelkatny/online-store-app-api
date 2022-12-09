179dba6ec48627396815576fdb287195b9591967
Paweł Kątny <pawel.katny@gmail.com>
2022-12-09 21:54:47 +0100

    Update  mailer service
    * update mailer service to get new access token before sending email

--- src/services/mailer.js
+++ src/services/mailer.js
@@ -1,16 +1,34 @@
-const { installed: { client_id, client_secret } } = require('../config/mailer/credentials.json');
+const { web: { client_id, client_secret, redirect_uris } } = require('../config/mailer/credentials.json');
 const { refresh_token, access_token, expiry_date } = require('../config/mailer/token.json');
 const path = require('path');
 const nodemailer = require('nodemailer');
+const { google } = require("googleapis");
+const OAuth2 = google.auth.OAuth2;
 const nmHandlebars = require('nodemailer-express-handlebars');
+const { threadId } = require('worker_threads');
 
 
 class MailerService {
     constructor() {
-        this.createTransporter();
+        this.clientId = client_id;
+        this.clientSecret = client_secret;
+        this.redirectUri = redirect_uris[0];
+        this.accessToken = access_token;
+        this.refreshToken = refresh_token;
+        this.expiryDate = expiry_date; 
     }
 
-    createTransporter() {
+    async getAccessToken() {
+        const oauth2Client = new OAuth2(this.clientId, this.clientSecret, this.redirectUri);
+        oauth2Client.setCredentials({
+            refresh_token: this.refreshToken
+        });
+
+        const res = await oauth2Client.getAccessToken();
+        return res.token;
+    }
+
+    async createTransporter() {
         const nhbs = nmHandlebars({
             viewEngine: {
                 defaultLayout: false
@@ -18,16 +36,19 @@ class MailerService {
             viewPath: path.resolve(__dirname, '../templates/email')
         });
 
+        const accessToken = await this.getAccessToken();
+        this.accessToken = accessToken;
+
         const transporter = nodemailer.createTransport({
-            service: 'Gmail',
+            service: 'gmail',
             auth: {
                 type: 'OAuth2',
                 user: 'pawel.katny.dev@gmail.com',
-                clientId: client_id,
-                clientSecret: client_secret,
-                refreshToken: refresh_token,
-                accessToken: access_token,
-                expires: expiry_date
+                clientId: this.clientId,
+                clientSecret: this.clientSecret,
+                refreshToken: this.refreshToken,
+                accessToken: this.accessToken,
+                expires: this.expiryDate
             }
         }, {
             from: 'Test account <pawel.katny.dev@gmail.com>'
