4449c8ad69fb76360ba2a8b0b4618143c2a6e06f
Paweł Kątny <pawel.katny@gmail.com>
2023-01-18 22:18:10 +0100

    Update mailer service
    * remove sending emails via Google App
    * add sending email via classic method: with user and password

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,191 +1,192 @@
-const { Customer, User } = require('../models/user');
-const CustomError = require('../error/customError');
-const { StatusCodes } = require('http-status-codes');
-const Role = require('../models/role');
-const MailerService = require('./mailer');
-const jwt = require('async-jsonwebtoken');
-const { jwt_secret } = require('../config');
-const { startSession } = require('../models/role');
+const { Customer, User } = require("../models/user");
+const CustomError = require("../error/customError");
+const { StatusCodes } = require("http-status-codes");
+const Role = require("../models/role");
+const MailerService = require("./mailer");
+const jwt = require("async-jsonwebtoken");
+const { jwt_secret } = require("../config");
 
 class AuthService {
+  static async register(userData) {
+    const { email, name, password } = userData;
+    let token;
+    let user = await Customer.findOne({ email: email });
 
-    static async register(userData) {
-        const { email, name, password } = userData;
-        let token;
-        let user = await Customer.findOne({ email: email });
-
-        if (user) {
-            throw new CustomError('Conflict', StatusCodes.CONFLICT);
-        }
-
-        const status = {
-            success: false,
-            code: StatusCodes.INTERNAL_SERVER_ERROR
-        }
-
-        const role = await Role.findOne({ name: 'customer' });
-
-        user = await Customer.create({ email, password, name, role: role._id });
-        if (user) {
-            await user.createActivationToken();
-            await user.save();
-        }
-
-        if (user.accountActivation.token) {
-            const mailer = new MailerService();
-            await mailer.createTransporter();
-            const emailOptions = {
-                header: {
-                    to: email,
-                    subject: 'Account activation'
-                },
-                template: 'account-activation',
-                context: {
-                    companyName: 'TESTING PRODUCT',
-                    name: user.name.first,
-                    activateLink: user.accountActivation.token
-                }
-            }
-            const emailStatus = await mailer.sendEmail(emailOptions);
-
-            if (emailStatus.messageId) {
-                status.success = true;
-                status.code = StatusCodes.CREATED;
-            }
-        }
-
-        return status;
+    if (user) {
+      throw new CustomError("Conflict", StatusCodes.CONFLICT);
     }
 
-    static async login(loginData) {
-        const { email, password } = loginData;
+    const status = {
+      success: false,
+      code: StatusCodes.INTERNAL_SERVER_ERROR,
+    };
 
-        const user = await User.findOne({ email: email })
-            .populate({
-                path: 'role',
-                model: 'Role'
-            });
+    const role = await Role.findOne({ name: "customer" });
 
-        if (!user) {
-            throw new CustomError('Unathorized', StatusCodes.UNAUTHORIZED);
-        }
-
-        if (user.role.name == 'customer' && !user.active) {
-            throw new CustomError('Account hasn`t been activated yet.', StatusCodes.UNAUTHORIZED);
-        }
-
-        const pwdMatch = await user.comparePwd(password);
-        if (!pwdMatch) {
-            throw new CustomError('Unathorized', StatusCodes.UNAUTHORIZED);
-        }
-
-        const token = await user.createToken();
-
-        return { user: { name: user.name }, token };
+    user = await Customer.create({ email, password, name, role: role._id });
+    if (user) {
+      await user.createActivationToken();
+      await user.save();
     }
 
-    static async verify(token) {
-        const decoded = await jwt.verify(token, jwt_secret);
-        return decoded;
-    }
-
-    static async sendResetPasswordEmail(email) {
-        const user = await User.findOne({ email: email });
-        const status = {
-            success: false,
-            msg: 'User does not exist.',
-            code: StatusCodes.NOT_FOUND
-        };
-
-        if (user) {
-            await user.createResetPwdToken();
-            await user.save();
-
-            const mailer = new MailerService();
-            const emailOptions = {
-                header: {
-                    to: email,
-                    subject: 'Password reset'
-                },
-                template: 'password-reset',
-                context: {
-                    companyName: 'TESTING PRODUCT',
-                    name: user.name.first,
-                    resetPwdUri: user.passwordReset.token
-                }
-            }
-            const emailStatus = await mailer.sendEmail(emailOptions);
-
-            if (emailStatus.messageId) {
-                status.success = true;
-                status.msg = 'Your password was reseted. Pleas check your email and follow further instructions.';
-                status.code = StatusCodes.OK;
-            }
-
-        }
-
-        return status;
-    }
-
-    static async resetPassword(userId, token, password) {
-        const status = {
-            success: false
-        }
-
-        const user = await User.findOne({
-            _id: userId,
-            "passwordReset.token": token
-        });
-
-        if (!user) {
-            status.msg = 'User does not exist.';
-            status.code = StatusCodes.NOT_FOUND;
-        }
-
-        if (user && ((new Date() - user.passwordReset.createdAt) / 1000) > 3600) {
-            status.msg = 'Password reset token expired. Please try again.';
-            status.code = StatusCodes.NOT_FOUND;
-        }
-
-        if (user && ((new Date() - user.passwordReset.createdAt) / 1000) <= 3600) {
-            user.password = password;
-            await user.save();
-            status.success = true;
-            status.msg = 'Password successfully reseted. Please login using new password.';
-            status.code = StatusCodes.OK;
-        }
-
-        return status;
-    }
-
-    static async activateAccount(token) {
-        const status = {
-            success: false,
-            code: StatusCodes.INTERNAL_SERVER_ERROR
-        }
-
-        const user = await Customer.findOne({
-            confirmed: false,
-            "accountActivation.token": token
-        });
-
-        if (!user) {
-            status.msg = 'User does not exist.';
-            status.code = StatusCodes.NOT_FOUND;
-        }
-
-        const currentDate = new Date();
-        const expireAt = currentDate.setFullYear(currentDate.getFullYear() + 100);
-        user.expireAt = expireAt;
-        user.active = true;
-        await user.save();
-
+    if (user.accountActivation.token) {
+      const mailer = new MailerService();
+      await mailer.createTransporter();
+      const emailOptions = {
+        header: {
+          to: email,
+          subject: "Account activation",
+        },
+        template: "account-activation",
+        context: {
+          companyName: "TESTING PRODUCT",
+          name: user.name.first,
+          activateLink: user.accountActivation.token,
+        },
+      };
+      const emailStatus = await mailer.sendEmail(emailOptions);
+      console.log({ emailStatus });
+      if (emailStatus.messageId) {
         status.success = true;
-        status.msg = 'Your account has been activated. You can now log in.';
-        status.code = StatusCodes.OK;
-
-        return status;
+        status.code = StatusCodes.CREATED;
+      }
     }
+
+    return status;
+  }
+
+  static async login(loginData) {
+    const { email, password } = loginData;
+
+    const user = await User.findOne({ email: email }).populate({
+      path: "role",
+      model: "Role",
+    });
+
+    if (!user) {
+      throw new CustomError("Unathorized", StatusCodes.UNAUTHORIZED);
+    }
+
+    if (user.role.name == "customer" && !user.active) {
+      throw new CustomError(
+        "Account hasn`t been activated yet.",
+        StatusCodes.UNAUTHORIZED
+      );
+    }
+
+    const pwdMatch = await user.comparePwd(password);
+    if (!pwdMatch) {
+      throw new CustomError("Unathorized", StatusCodes.UNAUTHORIZED);
+    }
+
+    const token = await user.createToken();
+
+    return { user: { name: user.name }, token };
+  }
+
+  static async verify(token) {
+    const decoded = await jwt.verify(token, jwt_secret);
+    return decoded;
+  }
+
+  static async sendResetPasswordEmail(email) {
+    const user = await User.findOne({ email: email });
+    const status = {
+      success: false,
+      msg: "User does not exist.",
+      code: StatusCodes.NOT_FOUND,
+    };
+
+    if (user) {
+      await user.createResetPwdToken();
+      await user.save();
+
+      const mailer = new MailerService();
+      const emailOptions = {
+        header: {
+          to: email,
+          subject: "Password reset",
+        },
+        template: "password-reset",
+        context: {
+          companyName: "TESTING PRODUCT",
+          name: user.name.first,
+          resetPwdUri: user.passwordReset.token,
+        },
+      };
+      const emailStatus = await mailer.sendEmail(emailOptions);
+
+      if (emailStatus.messageId) {
+        status.success = true;
+        status.msg =
+          "Your password was reseted. Pleas check your email and follow further instructions.";
+        status.code = StatusCodes.OK;
+      }
+    }
+
+    return status;
+  }
+
+  static async resetPassword(userId, token, password) {
+    const status = {
+      success: false,
+    };
+
+    const user = await User.findOne({
+      _id: userId,
+      "passwordReset.token": token,
+    });
+
+    if (!user) {
+      status.msg = "User does not exist.";
+      status.code = StatusCodes.NOT_FOUND;
+    }
+
+    if (user && (new Date() - user.passwordReset.createdAt) / 1000 > 3600) {
+      status.msg = "Password reset token expired. Please try again.";
+      status.code = StatusCodes.NOT_FOUND;
+    }
+
+    if (user && (new Date() - user.passwordReset.createdAt) / 1000 <= 3600) {
+      user.password = password;
+      await user.save();
+      status.success = true;
+      status.msg =
+        "Password successfully reseted. Please login using new password.";
+      status.code = StatusCodes.OK;
+    }
+
+    return status;
+  }
+
+  static async activateAccount(token) {
+    const status = {
+      success: false,
+      code: StatusCodes.INTERNAL_SERVER_ERROR,
+    };
+
+    const user = await Customer.findOne({
+      confirmed: false,
+      "accountActivation.token": token,
+    });
+
+    if (!user) {
+      status.msg = "User does not exist.";
+      status.code = StatusCodes.NOT_FOUND;
+    }
+
+    const currentDate = new Date();
+    const expireAt = currentDate.setFullYear(currentDate.getFullYear() + 100);
+    user.expireAt = expireAt;
+    user.active = true;
+    await user.save();
+
+    status.success = true;
+    status.msg = "Your account has been activated. You can now log in.";
+    status.code = StatusCodes.OK;
+
+    return status;
+  }
 }
 
 module.exports = AuthService;
--- src/services/mailer.js
+++ src/services/mailer.js
@@ -1,74 +1,52 @@
-const { web: { client_id, client_secret, redirect_uris } } = require('../config/mailer/credentials.json');
-const { refresh_token, access_token, expiry_date } = require('../config/mailer/token.json');
-const path = require('path');
-const nodemailer = require('nodemailer');
+const { user, password } = require("../config/mailer/credentials.json");
+const path = require("path");
+const nodemailer = require("nodemailer");
 const { google } = require("googleapis");
 const OAuth2 = google.auth.OAuth2;
-const nmHandlebars = require('nodemailer-express-handlebars');
-const { threadId } = require('worker_threads');
-
+const nmHandlebars = require("nodemailer-express-handlebars");
+const { threadId } = require("worker_threads");
 
 class MailerService {
-    constructor() {
-        this.clientId = client_id;
-        this.clientSecret = client_secret;
-        this.redirectUri = redirect_uris[0];
-        this.accessToken = access_token;
-        this.refreshToken = refresh_token;
-        this.expiryDate = expiry_date; 
-    }
+  constructor() {
+    this.user = user;
+    this.password = password;
+  }
 
-    async getAccessToken() {
-        const oauth2Client = new OAuth2(this.clientId, this.clientSecret, this.redirectUri);
-        oauth2Client.setCredentials({
-            refresh_token: this.refreshToken
-        });
+  async createTransporter() {
+    const nhbs = nmHandlebars({
+      viewEngine: {
+        defaultLayout: false,
+      },
+      viewPath: path.resolve(__dirname, "../templates/email"),
+    });
 
-        const res = await oauth2Client.getAccessToken();
-        return res.token;
-    }
+    const transporter = nodemailer.createTransport(
+      {
+        service: "gmail",
+        auth: {
+          user: this.user,
+          pass: this.password,
+        },
+      },
+      {
+        from: "Test account <pawel.katny.dev@gmail.com>",
+      }
+    );
 
-    async createTransporter() {
-        const nhbs = nmHandlebars({
-            viewEngine: {
-                defaultLayout: false
-            },
-            viewPath: path.resolve(__dirname, '../templates/email')
-        });
+    transporter.use("compile", nhbs);
+    this.transporter = transporter;
+  }
 
-        const accessToken = await this.getAccessToken();
-        this.accessToken = accessToken;
+  async sendEmail(options) {
+    const message = {
+      to: options.header.to,
+      subject: options.header.subject,
+      template: options.template,
+      context: { ...options.context },
+    };
 
-        const transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                type: 'OAuth2',
-                user: 'pawel.katny.dev@gmail.com',
-                clientId: this.clientId,
-                clientSecret: this.clientSecret,
-                refreshToken: this.refreshToken,
-                accessToken: this.accessToken,
-                expires: this.expiryDate
-            }
-        }, {
-            from: 'Test account <pawel.katny.dev@gmail.com>'
-        });
-
-        transporter.use('compile', nhbs);
-        this.transporter = transporter;
-    }
-
-    async sendEmail(options) {
-
-        const message = {
-            to: options.header.to,
-            subject: options.header.subject,
-            template: options.template,
-            context: { ...options.context }
-        }
-
-        return this.transporter.sendMail(message);
-    }
+    return this.transporter.sendMail(message);
+  }
 }
 
 module.exports = MailerService;
