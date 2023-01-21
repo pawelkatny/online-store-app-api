48aa92efa466b4ce631916fcd379982020a56853
Paweł Kątny <pawel.katny@gmail.com>
2023-01-21 22:44:22 +0100

    Update order creation process
    * creating order is no longer limited to loged in user of role 'customer'

--- src/api/v1/routes/order.js
+++ src/api/v1/routes/order.js
@@ -1,15 +1,12 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const router = Router();
-const orderCtr = require('../../../controllers/order');
-const { isAuthenticated, isCustomer } = require('../../../middleware/auth');
+const orderCtr = require("../../../controllers/order");
+const { isAuthenticated, isCustomer } = require("../../../middleware/auth");
 
 router.use(isAuthenticated);
 
-router.route('/')
-    .get(orderCtr.getOrders)
-    .post(isCustomer, orderCtr.createOrder);
+router.route("/").get(orderCtr.getOrders).post(orderCtr.createOrder);
 
-router.route('/:orderId')
-    .get(orderCtr.getOrder);
+router.route("/:orderId").get(orderCtr.getOrder);
 
 module.exports = router;
--- src/controllers/order.js
+++ src/controllers/order.js
@@ -1,51 +1,62 @@
-const CustomError = require('../error/customError');
-const OrderService = require('../services/order');
-const CustomerService = require('../services/customer');
-const { StatusCodes } = require('http-status-codes');
+const CustomError = require("../error/customError");
+const OrderService = require("../services/order");
+const CustomerService = require("../services/customer");
+const { StatusCodes } = require("http-status-codes");
 
 const getOrders = async (req, res) => {
-    const currentUser = req.user;
-    if (!currentUser.hasPermission('view-order')) {
-        throw new CustomError('Unauthorized', StatusCodes.UNAUTHORIZED);
-    }
-    const { query } = req;
-    const orders = await OrderService.getOrders(query);
+  const currentUser = req.user;
+  if (!currentUser.hasPermission("view-order")) {
+    throw new CustomError("Unauthorized", StatusCodes.UNAUTHORIZED);
+  }
+  const { query } = req;
+  const orders = await OrderService.getOrders(query);
 
-    res.status(StatusCodes.OK).json({ orders });
-}
+  res.status(StatusCodes.OK).json({ orders });
+};
 
 const getOrder = async (req, res) => {
-    const currentUser = req.user;
-    if (!currentUser.hasPermission('view-order')) {
-        throw new CustomError('Unauthorized', StatusCodes.UNAUTHORIZED);
-    }
-    const { orderId } = req.params;
-    const order = await OrderService.getOrder(orderId);
-    if (!order) {
-        throw new CustomError('Not found', StatusCodes.NOT_FOUND);
-    }
+  const currentUser = req.user;
+  if (!currentUser.hasPermission("view-order")) {
+    throw new CustomError("Unauthorized", StatusCodes.UNAUTHORIZED);
+  }
+  const { orderId } = req.params;
+  const order = await OrderService.getOrder(orderId);
+  if (!order) {
+    throw new CustomError("Not found", StatusCodes.NOT_FOUND);
+  }
 
-    res.status(StatusCodes.OK).json({ order });
-}
+  res.status(StatusCodes.OK).json({ order });
+};
 
 const createOrder = async (req, res) => {
-    const currentUser = req.user;
-    const { delivery, address, products } = req.body;
+  const currentUser = req.user;
+  const { delivery, address, products, name, email, id } = req.body;
+  let userData = { name, email, id };
 
-    const order = await OrderService.createOrder(currentUser.id, {
-        delivery, address, products
-    });
-    if (!order) {
-        throw new CustomError('Something went wrong', StatusCodes.INTERNAL_SERVER_ERROR);
-    }
+  if (currentUser.hasRole("customer")) {
+    userData = { ...currentUser };
+  }
 
-    await CustomerService.clearCart(currentUser.id);
+  const order = await OrderService.createOrder(userData, {
+    delivery,
+    address,
+    products,
+  });
 
-    res.status(StatusCodes.CREATED).json({ order });
-}
+  if (!order) {
+    throw new CustomError(
+      "Something went wrong",
+      StatusCodes.INTERNAL_SERVER_ERROR
+    );
+  }
+
+  await CustomerService.clearCart(currentUser.id);
+
+  res.status(StatusCodes.CREATED).json({ order });
+};
 
 module.exports = {
-    getOrders,
-    getOrder,
-    createOrder
-}
+  getOrders,
+  getOrder,
+  createOrder,
+};
