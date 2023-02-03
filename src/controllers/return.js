bf23ce23ad881474beeba5da46356d1b3d7cb989
Paweł Kątny <pawel.katny@gmail.com>
2023-02-03 21:54:49 +0100

    Update return model
    * change customer field to object with additional keys
    * change order field - add order number

--- src/controllers/return.js
+++ src/controllers/return.js
@@ -1,63 +1,74 @@
-const CustomError = require('../error/customError');
-const ReturnService = require('../services/return');
-const { StatusCodes } = require('http-status-codes');
+const CustomError = require("../error/customError");
+const ReturnService = require("../services/return");
+const { StatusCodes } = require("http-status-codes");
 
 const getReturns = async (req, res) => {
-    const currentUser = req.user;
-    if (!currentUser.hasPermission('view-return')) {
-        throw new CustomError('Unauthorized', StatusCodes.UNAUTHORIZED);
-    }
-    const { query } = req;
-    const queryObject = {};
-    const returns = await ReturnService.getReturns(queryObject);
+  const currentUser = req.user;
+  if (!currentUser.hasPermission("view-return")) {
+    throw new CustomError("Unauthorized", StatusCodes.UNAUTHORIZED);
+  }
+  const { query } = req;
+  const queryObject = {};
+  const returns = await ReturnService.getReturns(queryObject);
 
-    res.status(StatusCodes.OK).json({ returns });
-}
+  res.status(StatusCodes.OK).json({ returns });
+};
 
 const getReturn = async (req, res) => {
-    const currentUser = req.user;
-    if (!currentUser.hasPermission('view-return')) {
-        throw new CustomError('Unauthorized', StatusCodes.UNAUTHORIZED);
-    }
+  const currentUser = req.user;
+  console.log(currentUser);
+  if (!currentUser.hasPermission("view-return")) {
+    throw new CustomError("Unauthorized", StatusCodes.UNAUTHORIZED);
+  }
 
-    const { returnId } = req.params;
-    const returnDoc = await ReturnService.getReturn(returnId);
-    if (!returnDoc) {
-        throw new CustomError('Not found', StatusCodes.NOT_FOUND);
-    }
+  const { returnId } = req.params;
+  const returnDoc = await ReturnService.getReturn(returnId);
+  if (!returnDoc) {
+    throw new CustomError("Not found", StatusCodes.NOT_FOUND);
+  }
 
-    res.status(StatusCodes.OK).json({ returnDoc }); 
-}
+  res.status(StatusCodes.OK).json({ returnDoc });
+};
 
 const createReturn = async (req, res) => {
-    const currentUser = req.user;
-    const { orderId, products, reason } = req.body;
-    let customerId;
+  const currentUser = req.user;
+  const { orderId, products, reason } = req.body;
+  let customerId;
 
-    if (!currentUser.hasRole('customer') && !currentUser.hasPermission('edit-return')) {
-        throw new CustomError('Unauthorized', StatusCodes.UNAUTHORIZED);
-    } 
+  if (
+    !currentUser.hasRole("customer") &&
+    !currentUser.hasPermission("edit-return")
+  ) {
+    throw new CustomError("Unauthorized", StatusCodes.UNAUTHORIZED);
+  }
 
-    if (currentUser.hasRole('customer')) {
-        customerId = currentUser.id;
-    } 
+  if (currentUser.hasRole("customer")) {
+    customerId = currentUser.id;
+  }
 
-    if (currentUser.hasPermission('edit-return')) {
-        let { custId } = req.body;
-        customerId = custId;
-    } 
+  if (currentUser.hasPermission("edit-return")) {
+    let { custId } = req.body;
+    customerId = custId;
+  }
 
-    const newReturn = await ReturnService.createReturn(customerId, { orderId, products, reason });
+  const newReturn = await ReturnService.createReturn(customerId, {
+    orderId,
+    products,
+    reason,
+  });
 
-    if (!newReturn) {
-        throw new CustomError('Something went wrong', StatusCodes.INTERNAL_SERVER_ERROR);
-    }
+  if (!newReturn) {
+    throw new CustomError(
+      "Something went wrong",
+      StatusCodes.INTERNAL_SERVER_ERROR
+    );
+  }
 
-    res.status(StatusCodes.CREATED).json({ newReturn });
-}
+  res.status(StatusCodes.CREATED).json({ newReturn });
+};
 
 module.exports = {
-    getReturn,
-    getReturns,
-    createReturn
-}
+  getReturn,
+  getReturns,
+  createReturn,
+};
