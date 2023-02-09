c6136b7300f0d1e2afc32e9dcd4d02a795d630cf
Paweł Kątny <pawel.katny@gmail.com>
2023-02-09 22:14:48 +0100

    Remove mailer service module

--- src/api/v1/routes/admin.js
+++ src/api/v1/routes/admin.js
@@ -1,12 +1,10 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const router = Router();
-const { isAuthenticated } = require('../../../middleware/auth');
-const { dashboard, checkMailerConfig } = require('../../../controllers/admin');
+const { isAuthenticated } = require("../../../middleware/auth");
+const { dashboard, checkMailerConfig } = require("../../../controllers/admin");
 
 router.use(isAuthenticated);
 
-router.route('/dashboard').get(dashboard);
-
-router.route('/check-mailer-config').get(checkMailerConfig);
+router.route("/dashboard").get(dashboard);
 
 module.exports = router;
--- src/controllers/admin.js
+++ src/controllers/admin.js
@@ -1,40 +1,10 @@
-const { StatusCodes } = require('http-status-codes');
-const MailerService = require('../services/mailer');
-const CustomError = require('../error/customError');
+const { StatusCodes } = require("http-status-codes");
+const CustomError = require("../error/customError");
 
 const dashboard = async (req, res) => {
-    res.status(200).json({ user: req.user });
-}
-
-const checkMailerConfig = async (req, res) => {
-    const currentUser = req.user;
-    currentUser.email = 'pawel.katny@gmail.com'
-    if (!currentUser.hasRole('admin')) {
-        throw new CustomError('Unathorized', StatusCodes.UNAUTHORIZED);
-    }
-
-    const mailer = new MailerService();
-    const emailOptions = {
-        header: {
-            to: currentUser.email,
-            subject: 'Testing Mailer Configuration',
-        },
-        template: 'test',
-        context: {
-            name: currentUser.name.first
-        }
-    }
-
-    const email = await mailer.sendEmail(emailOptions);
-
-    if (!email) {
-        throw new CustomError('Something went wrong', StatusCodes.INTERNAL_SERVER_ERROR);
-    }
-
-    res.status(StatusCodes.OK).send();
-}
+  res.status(200).json({ user: req.user });
+};
 
 module.exports = {
-    dashboard,
-    checkMailerConfig
-}
+  dashboard,
+};
