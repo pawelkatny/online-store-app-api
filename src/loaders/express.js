347ff70b6755f7bfb622d64279f59110d640abc9
Paweł Kątny <pawel.katny@gmail.com>
2023-02-16 22:46:48 +0100

    Fix users loader

--- src/loaders/express.js
+++ src/loaders/express.js
@@ -1,48 +1,53 @@
-require('express-async-errors');
-const path = require('path');
-const express = require('express');
-const config = require('../config');
-const morgan = require('morgan');
-const rfs = require('rotating-file-stream');
-const logger = require('../services/logger');
-const PermissionsLoader = require('./permissions');
-const RolesLoader = require('./roles');
-const RoutesLoader = require('./routes');
-const errorHandler = require('../error/errorHandler');
+require("express-async-errors");
+const path = require("path");
+const express = require("express");
+const config = require("../config");
+const morgan = require("morgan");
+const rfs = require("rotating-file-stream");
+const logger = require("../services/logger");
+const PermissionsLoader = require("./permissions");
+const RolesLoader = require("./roles");
+const RoutesLoader = require("./routes");
+const UsersLoader = require("./users");
+const errorHandler = require("../error/errorHandler");
 
 class ExpressLoader {
-    constructor() {
-        const app = express();
+  constructor() {
+    const app = express();
 
-        //middleware
-        app.use(express.static(`${process.cwd()}/src/public`));
-        app.use(express.json());
-        app.use(morgan('combined', { stream: rfs.createStream(config.logs.access.file, {
-            interval: config.logs.access.interval,
-            path: config.logs.access.path
-        })}));
+    //middleware
+    app.use(express.static(`${process.cwd()}/src/public`));
+    app.use(express.json());
+    app.use(
+      morgan("combined", {
+        stream: rfs.createStream(config.logs.access.file, {
+          interval: config.logs.access.interval,
+          path: config.logs.access.path,
+        }),
+      })
+    );
 
+    //default roles and permissions
+    PermissionsLoader.loadDefaults();
+    RolesLoader.loadDefaults();
+    UsersLoader.loadDefaults();
 
-        //default roles and permissions
-        PermissionsLoader.loadDefaults();
-        RolesLoader.loadDefaults();
-        
-        //routes
-        RoutesLoader.init(app);
-        
-        //error handling
-        app.use(errorHandler);
-        //resource not found
+    //routes
+    RoutesLoader.init(app);
 
-        //start server
-        this.startServer(app);        
-    }
+    //error handling
+    app.use(errorHandler);
+    //resource not found
 
-    startServer(app) {
-        this.server = app.listen(config.port, () => {
-            logger.log('info', `Server listening on port ${config.port}`);
-        });
-    }
+    //start server
+    this.startServer(app);
+  }
+
+  startServer(app) {
+    this.server = app.listen(config.port, () => {
+      logger.log("info", `Server listening on port ${config.port}`);
+    });
+  }
 }
 
 module.exports = ExpressLoader;
--- src/loaders/users.js
+++ src/loaders/users.js
@@ -1,5 +1,5 @@
 const Role = require("../models/role");
-const User = require("../models/user");
+const { User } = require("../models/user");
 const defaultUsers = require("../config/users");
 const logger = require("../services/logger");
 
@@ -9,10 +9,17 @@ class RolesLoader {
     for (const u of defaultUsers) {
       try {
         const role = await Role.findOne({ name: u.role });
-        if (!role) {
-          const user = await User.create(u);
+        let user = await User.findOne({ email: u.email });
+        if (role && !user) {
+          const user = await User.create({
+            role: role._id,
+            email: u.email,
+            name: u.name,
+            password: u.password,
+          });
         }
       } catch (error) {
+        console.log(error);
         logger.warn(`Error while creating user in DB: ${u.role}.`);
       }
     }
