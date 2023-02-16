1d33d90c7e60390dba8f9f0ccc2304928789c215
Paweł Kątny <pawel.katny@gmail.com>
2023-02-16 22:15:36 +0100

    Add password generator helper

---
+++ src/helpers/passGenerator.js
@@ -0,0 +1,11 @@
+const CHARACTERS =
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+const generatePwd = () => {
+  let pwd;
+
+  for (let i = 0; i < 10; i++) {
+    pwd += CHARACTERS.charAt(Math.floor(Math.random() * CHARACTERS.length));
+  }
+  return pwd;
+};
