fc41e3f42a68cdcf49768b8ae98cc6a22c570cb1
Paweł Kątny <pawel.katny@gmail.com>
2023-02-16 21:36:33 +0100

    Update mongo models
    * remove unused fields

--- src/models/product.js
+++ src/models/product.js
@@ -1,60 +1,65 @@
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'Product name is required.'],
-        unique: true,
-        maxlength: 50
+  name: {
+    type: String,
+    required: [true, "Product name is required."],
+    unique: true,
+    maxlength: 50,
+  },
+  scientificName: {
+    type: String,
+    required: [true, "Product scientific name is required."],
+    maxlength: 200,
+  },
+  family: {
+    type: String,
+    required: [true, "Product family is required."],
+    maxlength: 50,
+  },
+  description: {
+    type: String,
+    required: [true, "Product description is required."],
+    maxlength: 10000,
+  },
+  additionalInfo: [
+    {
+      type: String,
+      maxlength: 500,
     },
-    scientificName: {
-        type: String,
-        required: [true, 'Product scientific name is required.'],
-        maxlength: 200
+  ],
+  tags: [
+    {
+      type: String,
+      maxlength: 30,
     },
-    family: {
-        type: String,
-        required: [true, 'Product family is required.'],
-        maxlength: 50
+  ],
+  quantity: Number,
+  price: Number,
+  rating: {
+    type: Number,
+    default: null,
+  },
+  reviews: [
+    {
+      rating: Number,
+      review: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Review",
+      },
     },
-    description: {
-        type: String,
-        required: [true, 'Product description is required.'],
-        maxlength: 10000
-    },
-    additionalInfo: [{
-        type: String,
-        maxlength: 500
-    }],
-    tags: [{
-        type: String,
-        maxlength: 30
-    }],
-    images: [String],
-    quantity: Number,
-    price: Number,
-    rating: {
-        type: Number,
-        default: null
-    },
-    reviews: [{
-        rating: Number,
-        review: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Review"
-        }
-    }]
+  ],
 });
 
 productSchema.methods.updateRating = async function () {
-    const reviews = this.reviews;
-    const sumRating = reviews.reduce((a, b) => {
-        return a + b.rating;
-    }, 0);
+  const reviews = this.reviews;
+  const sumRating = reviews.reduce((a, b) => {
+    return a + b.rating;
+  }, 0);
 
-    this.rating = Number.parseFloat(sumRating/reviews.length).toFixed(1);
-}
+  this.rating = Number.parseFloat(sumRating / reviews.length).toFixed(1);
+};
 
-const Product = mongoose.model('Product', productSchema);
+const Product = mongoose.model("Product", productSchema);
 
 module.exports = Product;
