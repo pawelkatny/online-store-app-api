d9fab0b5a8a532c27e4b72a58fbdeee71540c8cb
Paweł Kątny <pawel.katny@gmail.com>
2023-02-17 22:05:37 +0100

    Remove admin routes and controller

--- src/loaders/routes.js
+++ src/loaders/routes.js
@@ -1,29 +1,27 @@
-const indexRoutes = require('../api/v1/routes/index');
-const authRoutes = require('../api/v1/routes/auth');
-const adminRoutes = require('../api/v1/routes/admin');
-const userRoutes = require('../api/v1/routes/user');
-const productRoutes = require('../api/v1/routes/product');
-const orderRoutes = require('../api/v1/routes/order');
-const returnRoutes = require('../api/v1/routes/return');
-const CustomError = require('../error/customError');
-const { api: {
-    prefix
-} } = require('../config');
+const indexRoutes = require("../api/v1/routes/index");
+const authRoutes = require("../api/v1/routes/auth");
+const userRoutes = require("../api/v1/routes/user");
+const productRoutes = require("../api/v1/routes/product");
+const orderRoutes = require("../api/v1/routes/order");
+const returnRoutes = require("../api/v1/routes/return");
+const CustomError = require("../error/customError");
+const {
+  api: { prefix },
+} = require("../config");
 
 class RoutesLoader {
-    static init(app) {
-        app.use(`${prefix}`, indexRoutes);
-        app.use(`${prefix}/auth`, authRoutes);
-        app.use(`${prefix}/admin`, adminRoutes);
-        app.use(`${prefix}/users`, userRoutes);
-        app.use(`${prefix}/products`, productRoutes);
-        app.use(`${prefix}/orders`, orderRoutes);
-        app.use(`${prefix}/returns`, returnRoutes);
-        app.use('*', (req, res, next) => {
-            const err = new CustomError('Not found', 404);
-            next(err);
-        })
-    }
+  static init(app) {
+    app.use(`${prefix}`, indexRoutes);
+    app.use(`${prefix}/auth`, authRoutes);
+    app.use(`${prefix}/users`, userRoutes);
+    app.use(`${prefix}/products`, productRoutes);
+    app.use(`${prefix}/orders`, orderRoutes);
+    app.use(`${prefix}/returns`, returnRoutes);
+    app.use("*", (req, res, next) => {
+      const err = new CustomError("Not found", 404);
+      next(err);
+    });
+  }
 }
 
 module.exports = RoutesLoader;
