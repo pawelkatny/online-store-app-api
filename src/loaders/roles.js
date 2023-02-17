04eefd688a2c1805bb4dc6b30fb735b386a74b6d
Paweł Kątny <pawel.katny@gmail.com>
2023-02-17 22:05:37 +0100

    Remove admin routes and controller

--- src/loaders/roles.js
+++ src/loaders/roles.js
@@ -1,23 +1,25 @@
-const Permission = require('../models/permission');
-const Role = require('../models/role');
-const defaultRoles = require('../config/roles');
-const logger = require('../services/logger');
+const Permission = require("../models/permission");
+const Role = require("../models/role");
+const defaultRoles = require("../config/roles");
+const logger = require("../services/logger");
 
 class RolesLoader {
-    static async loadDefaults() {
-        logger.info('Loading default roles to DB...');
-        for (const r of defaultRoles) {
-            try {
-                const permissions = await Permission.find({ 'tag' : { $in: r.permissions }});
-                let role = await Role.findOne({ name: r.name });
-                if (!role) {
-                    role = await Role.create( { name: r.name, permissions: permissions });
-                }
-            } catch (error) {
-                logger.warn(`Error while creating role in DB: ${r.name}.`);
-            }
+  static async loadDefaults() {
+    logger.info("Loading default roles to DB...");
+    for (const r of defaultRoles) {
+      try {
+        const permissions = await Permission.find({
+          tag: { $in: r.permissions },
+        });
+        let role = await Role.findOne({ name: r.name });
+        if (!role) {
+          role = await Role.create({ name: r.name, permissions: permissions });
         }
+      } catch (error) {
+        logger.warn(`Error while creating role in DB: ${r.name}.`);
+      }
     }
+  }
 }
 
 module.exports = RolesLoader;
